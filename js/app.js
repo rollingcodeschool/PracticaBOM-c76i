f8216923e18a3a1dd686f386d419f1193aea50bf
Emilse Arias <emilsearias@gmail.com>
2023-11-23 17:14:12 -0300

    agregamos un ejemplo de setInterval

--- js/app.js
+++ js/app.js
@@ -5,10 +5,17 @@ function saludar(){
 }
 
 // window.setTimeout( saludar, 5000);
-setTimeout( saludar, 5000);
-setTimeout( ()=>{
-    document.write(`<p>Hoy es un dia de lluvia 🌧️ </p>`)
-}, 6000);
+// setTimeout( saludar, 5000);
+// setTimeout( ()=>{
+//     document.write(`<p>Hoy es un dia de lluvia 🌧️ </p>`)
+// }, 6000);
 
 
 // setInterval
+ const identificadorIntervalo = setInterval(saludar, 2000);
+ console.log(identificadorIntervalo)
+
+ setTimeout(()=>{
+    console.log('ahora detengo el setInterval') 
+    clearInterval(identificadorIntervalo)
+ }, 10000);
